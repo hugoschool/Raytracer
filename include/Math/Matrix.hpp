434911c21d30b34f89805f0938282b42e8406105
Nadal <nadal.berthelon@epitech.eu>
2026-04-21 16:03:04 +0200

    [FEAT] matrix class with rotate methods

--- include/Math/Matrix.hpp
+++ include/Math/Matrix.hpp
@@ -14,15 +14,15 @@ namespace Raytracer {
         class Matrix3x3 {
             public:
                 Matrix3x3();
-                Matrix3x3(std::array<double, 9>);
-                Matrix3x3(std::array<std::array<double, 3>, 3>);
+                Matrix3x3(double, double, double, double, double, double, double, double, double);
+                Matrix3x3(std::array<std::array<double, 3>, 3> &);
 
-                Matrix3x3(Vector3D);
+                Matrix3x3(Vector3D &);
                 ~Matrix3x3() = default;
                 Matrix3x3 rotateX(double angle);
                 Matrix3x3 rotateY(double angle);
                 Matrix3x3 rotateZ(double angle);
-                Matrix3x3 operator*(Matrix3x3);
+                Matrix3x3 operator*(Matrix3x3 &);
             private:
                 std::array<std::array<double, 3>, 3> _matrix;
         };
--- src/Math/Matrix.cpp
+++ src/Math/Matrix.cpp
@@ -1,6 +1,7 @@
 #include "Math/Matrix.hpp"
 #include "Math/Vector3D.hpp"
 #include <array>
+#include <cmath>
 
 Raytracer::Math::Matrix3x3::Matrix3x3()
 {
@@ -9,25 +10,25 @@ Raytracer::Math::Matrix3x3::Matrix3x3()
     this->_matrix[2] = {0, 0, 0};
 }
 
-Raytracer::Math::Matrix3x3::Matrix3x3(std::array<double, 9> arr)
+Raytracer::Math::Matrix3x3::Matrix3x3(double a, double b, double c, double d, double e, double f, double g, double h, double i)
 {
-    this->_matrix[0] = {arr[0], arr[1], arr[2]};
-    this->_matrix[1] = {arr[3], arr[4], arr[5]};
-    this->_matrix[2] = {arr[6], arr[7], arr[9]};
+    this->_matrix[0] = {a, b, c};
+    this->_matrix[1] = {d, e, f};
+    this->_matrix[2] = {g, h, i};
 }
 
-Raytracer::Math::Matrix3x3::Matrix3x3(Raytracer::Math::Vector3D vector)
+Raytracer::Math::Matrix3x3::Matrix3x3(Raytracer::Math::Vector3D &vector)
 {
     this->_matrix[0] = {vector.x, 0, 0};
     this->_matrix[1] = {0, vector.y, 0};
     this->_matrix[2] = {0, 0, vector.z};
 }
 
-Raytracer::Math::Matrix3x3::Matrix3x3(std::array<std::array<double, 3>, 3> arr) : _matrix(arr)
+Raytracer::Math::Matrix3x3::Matrix3x3(std::array<std::array<double, 3>, 3> &arr) : _matrix(arr)
 {
 }
 
-Raytracer::Math::Matrix3x3 Raytracer::Math::Matrix3x3::operator*(Matrix3x3 other)
+Raytracer::Math::Matrix3x3 Raytracer::Math::Matrix3x3::operator*(Matrix3x3 &other)
 {
     std::array<std::array<double, 3>, 3> result;
 
@@ -40,4 +41,34 @@ Raytracer::Math::Matrix3x3 Raytracer::Math::Matrix3x3::operator*(Matrix3x3 other
         }
     }
     return Matrix3x3(result);
+}
+
+Raytracer::Math::Matrix3x3 Raytracer::Math::Matrix3x3::rotateX(double angle)
+{
+    Matrix3x3 Rotation_matrix(
+        1, 0, 0,
+        0, std::round(std::cos(angle)), std::round(-std::sin(angle)),
+        0, std::round(std::sin(angle)), std::round(std::cos(angle))
+    );
+    return *this * Rotation_matrix;
+}
+
+Raytracer::Math::Matrix3x3 Raytracer::Math::Matrix3x3::rotateY(double angle)
+{
+    Matrix3x3 Rotation_matrix(
+        std::round(std::cos(angle)), 0, std::round(std::sin(angle)),
+        0, 1, 0,
+        std::round(-std::sin(angle)), 0, std::round(std::cos(angle))
+    );
+    return *this * Rotation_matrix;
+}
+
+Raytracer::Math::Matrix3x3 Raytracer::Math::Matrix3x3::rotateZ(double angle)
+{
+    Matrix3x3 Rotation_matrix(
+        std::round(std::cos(angle)), std::round(-std::sin(angle)), 0,
+        std::round(std::sin(angle)), std::round(std::cos(angle)), 0,
+        0, 0, 1
+    );
+    return *this * Rotation_matrix;
 }
